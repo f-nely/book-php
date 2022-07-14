a04b5899e1c9c4324cbeeb29429d70b1420e29f0
nely de faria <nelly.nf54@gmail.com>
2022-07-14 12:03:00 -0300

    Cria arquivo constantes.php

---
+++ capitulo-1/constantes.php
@@ -0,0 +1,15 @@
+<?php
+
+/* Constantes
+Uma constante é um valor que não sofre modicações durante a execução
+do programa. Ela é representada por um identicador, assim como as
+variáveis, com a exceção de que só pode conter valores escalares
+(booleano, inteiro, ponto utuante e string) ou arrays. As regras de
+nomenclatura de constantes são as mesmas regras das variáveis, exceto
+pelo fato de as constantes não serem precedidas pelo sinal de cifrão ( $ ) e
+geralmente utilizarem nomes em letras maiúsculas.
+*/
+
+// define('MAXIMO_CLIENTES', 100);
+const MAXIMO_CLIENTES = 100;
+echo MAXIMO_CLIENTES;
