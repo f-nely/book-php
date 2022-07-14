833c948bf3a6f655f271dc633888940ec5f66781
nely de faria <nelly.nf54@gmail.com>
2022-07-14 12:06:08 -0300

    Cria arquivo operadores-atribuicao.php

---
+++ capitulo-1/operadores-atribuicao.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Operadores de atribuição
+Um operador de atribuição é usado para denir o valor de uma variável.
+O operador básico de atribuição é = , mas outros operadores podem ser
+utilizados:
+*/
+
+$var = 100;
+$var += 5;
+$var -= 5;
+$var *= 5;
+$var /= 5;
+echo $var;
