408fcdef751126bbbf0a86e3e8873c5f3a6686bd
nely de faria <nelly.nf54@gmail.com>
2022-07-21 16:22:40 -0300

    Cria arquivo if-else.php

---
+++ capitulo-1/if-else.php
@@ -0,0 +1,17 @@
+<?php
+
+$a = 1;
+
+if ($a == 5) {
+    echo 'é igual<br>';
+} else {
+    echo 'não é igual<br>';
+}
+
+if (isset($a)) {
+    echo '$a tem conteúdo<br>';
+}
+
+if (isset($b)) {
+    echo '$b tem conteúdo<br>';
+}
