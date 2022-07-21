b366a5d84b7eab506d6fd5a0295d69438951aa68
nely de faria <nelly.nf54@gmail.com>
2022-07-21 16:24:47 -0300

    Cria arquivo switch.php

---
+++ capitulo-1/switch.php
@@ -0,0 +1,16 @@
+<?php
+
+$i = 1;
+switch ($i) {
+    case 0:
+        echo 'i é igual a 0';
+        break;
+    case 1:
+        echo 'i é igual a 1';
+        break;
+    case 2:
+        echo 'i é igual a 2';
+        break;
+    default:
+        echo 'i não é igual a 0, 1 ou 2';
+}
