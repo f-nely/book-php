276c90762451afa230239159ce15299d7743c2b5
nely de faria <nelly.nf54@gmail.com>
2022-07-22 16:44:54 -0300

    Cria arquivo str-replace.php

---
+++ capitulo-1/str-replace.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+STR_REPLACE
+Substitui uma string (primeiro parâmetro) por outra (segundo
+parâmetro) dentro de um dado contexto (terceiro parâmetro).
+mixed str_replace (mixed $search , mixed $replace , mixed $subject
+[, int &$count ])
+*/
+
+echo str_replace('Rato', 'Leão', 'O Rato roeu a roupa do rei de Roma');
