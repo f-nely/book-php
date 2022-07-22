13a42da6d8f77ad4c9ef512487d3752db2c1ce0c
nely de faria <nelly.nf54@gmail.com>
2022-07-22 16:40:47 -0300

    Cria arquivo str-repeat.php

---
+++ capitulo-1/str-repeat.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+STR_REPEAT
+Repete uma string por determinada quantidade de vezes.
+string str_repeat (string $input , int $multiplier)
+*/
+
+$txt = ' .oO0Oo. ';
+echo str_repeat($txt, 5);
