e10b695235e1bc0b3cf52ac1bcebafa7dbb9fcc4
nely de faria <nelly.nf54@gmail.com>
2022-09-27 17:52:26 -0300

    Cria metodo-abstrato.php

---
+++ capitulo-2/metodo-abstrato.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once 'classes/Conta.php';
+
+class ContaSalario extends Conta
+{
+    /*
+        Fatal error: Class ContaSalario contains 1 abstract method and must therefore be declared abstract or implement the remaining methods (Conta::retirar)
+    */
+}
