4a269143b6ae455a22fc9f60c63b2730bf74113a
nely de faria <nelly.nf54@gmail.com>
2022-09-27 17:52:01 -0300

    Cria classe-final.php

---
+++ capitulo-2/classe-final.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'classes/Conta.php';
+require_once 'classes/ContaPoupanca.php';
+
+class ContaPoupancaUniversitaria extends ContaPoupanca
+{
+    // Fatal error: Class ContaPoupancaUniversitaria cannot extend final class ContaPoupanca
+}
